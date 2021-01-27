3d5e878ed603a01efe77311498364a7eb5a98c85
depsterr <depsterr@protonmail.com>
2021-01-27 23:42:59 +0100

    use pkg-config for configuring x11

--- configure.sh
+++ configure.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 if command -V pkg-config >/dev/null 2>&1; then
-	printf '%s\n' "CONFIG=$(pkg-config libmpdclient --libs --cflags || exit
-	) $(pkg-config imlib2 --libs --cflags || exit
-	) -lX11" > config.mk
+	printf '%s\n' "CONFIG=$(
+	pkg-config libmpdclient --libs --cflags) $(
+	pkg-config imlib2 --libs --cflags) $(
+	pkg-config x11 --libs --cflags)" > config.mk
 else
 	printf '%s\n' "CONFIG=-lmpdclient -lImlib2 -lX11" > config.mk
 fi
